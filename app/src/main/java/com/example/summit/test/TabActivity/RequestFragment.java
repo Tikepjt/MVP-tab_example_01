54d0bf97a440d867a9a570b6cf5c0667021d81cd
Unknown <Tikepjt@gmail.com>
2018-08-10 01:30:45 +0900

    18.08.10-Home-Commit

--- app/src/main/java/com/example/summit/test/TabActivity/RequestFragment.java
+++ app/src/main/java/com/example/summit/test/TabActivity/RequestFragment.java
@@ -1,4 +1,24 @@
 package com.example.summit.test.TabActivity;
 
-public class RequestFragment {
+import android.app.Fragment;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.summit.test.R;
+import com.example.summit.test.databinding.FragmentRequestBinding;
+
+public class RequestFragment extends Fragment {
+    FragmentRequestBinding mBinding;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle saveInstanceState) {
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_request, container, true);
+
+        return mBinding.getRoot();
+    }
 }
--- app/src/main/java/com/example/summit/test/TabActivity/TabActivity.java
+++ app/src/main/java/com/example/summit/test/TabActivity/TabActivity.java
@@ -1,17 +1,54 @@
 package com.example.summit.test.TabActivity;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.example.summit.test.R;
+import com.example.summit.test.databinding.ActivityTabBinding;
+
 
 public class TabActivity extends AppCompatActivity {
 
+    ActivityTabBinding mBinding;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tab);
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_tab);
 
 
+        mBinding.tabRequestLayout.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mBinding.tabFriendsLayout.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+
 }
+
+
+//
+//public class TabActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_tab);
+//
+//
+//    }
+//}
+
